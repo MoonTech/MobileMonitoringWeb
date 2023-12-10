a3bc46ab5efcd0e32696c47faa2e45afaab4264a
magulloff <przemczar2001@gmail.com>
2023-12-11 00:34:24 +0100

    fdsafdsafdsa

--- src/mutations/createRoom.ts
+++ src/mutations/createRoom.ts
@@ -1,20 +1,23 @@
 import { useMutation } from "react-query";
+import { useCache } from "../contexts/dataCacheContext";
 import { SERVER_URL } from "../serverUrl";
 import { PostRoomRequest } from "../types/postRoomRequest";
 import { PostRoomResponse } from "../types/postRoomResponse";
 
 export const useCreateRoom = () => {
+  const { userData } = useCache();
   const { mutateAsync } = useMutation(
     async (request: PostRoomRequest) => {
       return fetch(SERVER_URL + "room", {
         method: "POST",
         body: JSON.stringify(request),
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          authorization: `Bearer ${userData?.token}`,
         },
       })
         .then(res => res.json())
-        .then(res => (res as PostRoomResponse).accessToken)
+        .then(res => (res as PostRoomResponse).roomName)
     },
     {
       onSuccess: async () => {
